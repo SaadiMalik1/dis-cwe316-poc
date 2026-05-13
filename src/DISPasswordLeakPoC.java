e8ec8f16eccb72eeaf504f7674fb93ed6085113d
Ronin <saadmalik12148@gmail.com>
2026-05-13 17:54:40 +0500

    fix: add missing javax.management.MBeanServer import

--- src/DISPasswordLeakPoC.java
+++ src/DISPasswordLeakPoC.java
@@ -22,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.security.*;
 import java.util.*;
+import javax.management.MBeanServer;
 
 public class DISPasswordLeakPoC {
 
